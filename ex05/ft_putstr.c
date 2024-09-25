189dae6658bf7b2cc2c66fe44f056612417dcc4f
Biraj Phuyal <bphuyal@c1r3s4.42lisboa.com>
2024-09-25 12:50:48 +0100

    add

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -1,34 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bphuyal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 18:44:03 by bphuyal           #+#    #+#             */
-/*   Updated: 2024/09/23 20:27:28 by bphuyal          ###   ########.fr       */
+/*   Created: 2024/09/23 20:28:05 by bphuyal           #+#    #+#             */
+/*   Updated: 2024/09/25 10:42:32 by bphuyal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+void	ft_putstr(char *str)
 {
-	int t = *a / *b;
-	int c = *a % *b;
-	*a = t;
-	*b = c;
-}
-/*
-int main()
-{
-	int a = 11;
-	int b = 5;
-	int *t = &a;
-	int *c = &b;
-	ft_ultimate_div_mod(t, c);
-	printf("%d\n", a);
-	printf("%d\n", b);
+	int i = 0;
 
+	while(str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+
+		i++;
+	}
 }
-*/
+
+int	main()
+{
+	char str[] = "dog";
+	ft_putstr(str);
+}
--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -1,34 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bphuyal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 18:44:03 by bphuyal           #+#    #+#             */
-/*   Updated: 2024/09/23 20:27:28 by bphuyal          ###   ########.fr       */
+/*   Created: 2024/09/25 10:59:53 by bphuyal           #+#    #+#             */
+/*   Updated: 2024/09/25 12:47:50 by bphuyal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+void	ft_rev_int_tab(int *tab, int size)
 {
-	int t = *a / *b;
-	int c = *a % *b;
-	*a = t;
-	*b = c;
+	while (tab > size)
+	{
+		tab++;
+	}
 }
-/*
+
 int main()
 {
-	int a = 11;
-	int b = 5;
-	int *t = &a;
-	int *c = &b;
-	ft_ultimate_div_mod(t, c);
-	printf("%d\n", a);
-	printf("%d\n", b);
-
+	int A[] = {1,2,3,4,5};
+	int a = sizeof(A);
+	ft_rev_int_tab(A, a);
 }
-*/
